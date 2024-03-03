bd6c29060f2164c0614f60a93fd23532beca6020
mayzabar26 <mayzabar26@gmail.com>
2024-03-02 21:33:27 -0300

    W01 Programming Tasks

--- w01-task.js
+++ w01-task.js
@@ -6,8 +6,8 @@ userName = "Moronihah";
 console.log(`Username: ${userName}`);
 
 // 🔍 Part 2 error
-const now = new Date();
-const currentDateAndTime = Date();
+const now = new Date(); //adding const now = new Date();
+const currentDateAndTime = Date(); //taking out const currentDateAndTime = DateTime; and adding const currentDateAndTime = Date(); 
 console.log(`It is now ${currentDateAndTime}`);
 
 // 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
@@ -18,7 +18,7 @@ console.log(`The total is ${theTotal}`);
 // 'total' function declaration
 function total(...theNumbers) {
 	let sum = 0;
-	for (let aNumber of theNumbers) {
+	for (let aNumber of theNumbers) { //replacing in with of 
 		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
 	}
 	return sum
